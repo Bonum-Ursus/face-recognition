b69e2e1718a5a622386dc58422a296c564cc908b
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2020-01-31 19:40:14 +0500

    Updates

---
+++ precache-manifest.ba74ca5ab90e5ce11801180d0b46df27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07152bcd0bae8ea38449fd97e3f4c55a",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "4e5a8327860cd6729b10",
+    "url": "/face-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "51d6eaeb0c5b72538ffe",
+    "url": "/face-recognition/static/css/main.9d61963f.chunk.css"
+  },
+  {
+    "revision": "4e5a8327860cd6729b10",
+    "url": "/face-recognition/static/js/2.cbd1549c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/face-recognition/static/js/2.cbd1549c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d6eaeb0c5b72538ffe",
+    "url": "/face-recognition/static/js/main.07affb00.chunk.js"
+  },
+  {
+    "revision": "522c13753536d5926ec8",
+    "url": "/face-recognition/static/js/runtime-main.5b3babd5.js"
+  },
+  {
+    "revision": "23055e6549c174659ebb9aa9acba75fd",
+    "url": "/face-recognition/static/media/LogoImage.23055e65.png"
+  }
+]);
