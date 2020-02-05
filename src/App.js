44bb18adf68b71e199fcad75c14433a5e0209f07
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2020-02-05 16:28:30 +0500

    FaceBox added

--- src/App.js
+++ src/App.js
@@ -47,9 +47,30 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageURL: ''
+      imageURL: '',
+      box: {}
     }
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottom: height - (clarifaiFace.bottom_row * height)
+    
+    }
+  }
+// https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlIGlYgQzCtfNqvfB6A27prEg3xrgx3_H9mulYHr-6q0wwpob5
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
   onInputChange = (event) => {
     this.setState({input: event.target.value})
   }
@@ -60,14 +81,8 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(
-      function(response) {
-            console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-          },
-    function(err) {
-      // there was an error
-    }
-  );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render(){
@@ -79,7 +94,7 @@ class App extends Component {
           <Logo />
           <Rank />
           <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-          <FaceRecognition imageURL={this.state.imageURL}/>
+          <FaceRecognition box={this.state.box} imageURL={this.state.imageURL}/>
         </div>
         <div>
           
--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -3,10 +3,18 @@ import './FaceRecognition.css'
 
 
 
-const FaceRecognition = ({imageURL}) => {
+const FaceRecognition = ({imageURL, box}) => {
     return (
         <div className='center'>
-            <img className='image mt2' alt='' src={imageURL} />
+            <div className='absolute mt2'>
+                <img id='inputImage' className='image mt2' alt='' src={imageURL}/>
+                <div className='bounding-box' 
+                    style={{top: box.topRow, 
+                            right: box.rightCol, 
+                            left: box.leftCol, 
+                            bottom: box.bottom}}>
+                </div>
+            </div>
         </div>
     );
 }
